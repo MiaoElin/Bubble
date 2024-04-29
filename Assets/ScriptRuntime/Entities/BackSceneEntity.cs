86ca5fa87cda8f02d97c42a74f961cc870482eba
MiaoElin <249176332@qq.com>
2024-04-29 14:06:11 +0800

    Establish

---
+++ Assets/ScriptRuntime/Entities/BackSceneEntity.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class BackSceneEntity : MonoBehaviour
+{
+
+    [SerializeField] Sprite spr_BG;
+    [SerializeField] Sprite spr_Middle;
+    [SerializeField] Sprite spr_Spawner;
+
+    public BackSceneEntity(){
+
+    }
+    
+}
---
+++ Assets/ScriptRuntime/Entities/BubbleEntity.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class BubbleEntity : MonoBehaviour
+{
+
+    public Color color;
+
+}
